5222ded264f14537f47df0368848356098d01a08
Patryk Kopeć <Margok1234@gmail.com>
2018-08-26 22:14:35 +0200

    Initial commit

---
+++ Source/Catcher/CatcherGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CatcherGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CATCHER_API ACatcherGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
